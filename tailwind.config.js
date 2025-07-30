145bc9830761af77a9ca0ab5213dd8cea1e30486
Xuan-Minh <naux.pro@gmail.com>
2025-07-30 12:05:39 +0200

    Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { u } from 'framer-motion/client';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,8 +7,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        unbounded: ['Unbounded', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        amanojaku: ['Amanojaku', 'sans-serif']
+    },
   },
   plugins: [],
 }
-
+}
