c300714793fba36c240ef6d9957cc32803aa71f5
Xuan-Minh <naux.pro@gmail.com>
2025-07-30 11:03:04 +0200

    Reset

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center text-white">
+      <h1 className="text-5xl font-bold mb-4">À Propos</h1>
+      <p className="text-xl">Ceci est la page "À Propos".</p>
+    </div>
+  );
+};
+
+export default AboutPage;
