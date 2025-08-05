f0a34a20113bc301bc14cd67f8f27323a8302fa5
Xuan-Minh <naux.pro@gmail.com>
2025-08-05 20:14:53 +0200

    latest post

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,23 +17,18 @@ export default {
         'amanojaku': ['Amanojaku', 'sans-serif'],
       },
       // NOUVEAU: Définition de l'animation et des keyframes
-      animation: {
-        marquee: 'marquee 25s linear infinite',
-        // NOUVEAU: Animation pour une boucle parfaite avec texte dupliqué
-        'marquee-full': 'marquee-full 40s linear infinite',
+        animation: {
+        marquee: 'marquee 40s linear infinite',
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        // NOUVEAU: Keyframes pour la nouvelle animation
-        'marquee-full': {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-50%)' },
         },
       },
     },
   },
-  plugins: [],
+  plugins: [
+ require('tailwind-scrollbar-hide')
+],
 }
