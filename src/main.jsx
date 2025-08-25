949c7ff0aba8d62b502457132fc291851fff6d92
Xuan-Minh <naux.pro@gmail.com>
2025-08-25 21:59:57 +0200

    Ajout du HelmetProvider pour la gestion des métadonnées du document

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter} from 'react-router-dom';
 import App from './App.jsx'
+import { HelmetProvider } from "react-helmet-async";
 import './index.css'
 
 import './i18n';  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <HelmetProvider>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 )
